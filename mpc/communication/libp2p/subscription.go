f34222d2364c66593cde75b76f8fd0eef928d50c
Mak Muftic <mak@chainsafe.io>
2022-03-31 09:46:45 +0200

    Add session subscriber with unit tests

---
+++ mpc/communication/libp2p/subscription.go
@@ -0,0 +1,72 @@
+package libp2p
+
+import (
+	comm "github.com/ChainSafe/chainbridge-core/mpc/communication"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
+)
+
+// SessionSubscriptionManager manages channel subscriptions by comm.SessionID
+type SessionSubscriptionManager struct {
+	lock        *sync.Mutex
+	r           rand.Source
+	subscribers map[comm.SessionID]map[comm.SubscriptionID]chan *comm.WrappedMessage
+}
+
+func NewSessionSubscriptionManager() *SessionSubscriptionManager {
+	return &SessionSubscriptionManager{
+		lock:        &sync.Mutex{},
+		r:           rand.NewSource(time.Now().Unix()),
+		subscribers: make(map[comm.SessionID]map[comm.SubscriptionID]chan *comm.WrappedMessage),
+	}
+}
+
+// GetSubscribers return all subscribers for specific session
+func (ms *SessionSubscriptionManager) GetSubscribers(
+	sessionID comm.SessionID,
+) []chan *comm.WrappedMessage {
+	ms.lock.Lock()
+	defer ms.lock.Unlock()
+	subsAsMap, ok := ms.subscribers[sessionID]
+	if !ok {
+		return nil
+	}
+	var subsAsArray []chan *comm.WrappedMessage
+	for _, sub := range subsAsMap {
+		subsAsArray = append(subsAsArray, sub)
+	}
+	return subsAsArray
+}
+
+// Subscribe adds provided channel to session subscribers.
+// Returns SubscriptionID that is unique identifier of this subscription and is needed to UnSubscribe.
+func (ms *SessionSubscriptionManager) Subscribe(
+	sessionID comm.SessionID, channel chan *comm.WrappedMessage,
+) comm.SubscriptionID {
+	ms.lock.Lock()
+	defer ms.lock.Unlock()
+	// create subscription id
+	subID := comm.SubscriptionID(strconv.FormatInt(ms.r.Int63(), 10))
+	sessionSubscribers, ok := ms.subscribers[sessionID]
+	if !ok {
+		sessionSubscribers = map[comm.SubscriptionID]chan *comm.WrappedMessage{}
+	}
+	sessionSubscribers[subID] = channel
+	ms.subscribers[sessionID] = sessionSubscribers
+	return subID
+}
+
+// UnSubscribe a specific subscription, defined by SubscriptionID
+func (ms *SessionSubscriptionManager) UnSubscribe(
+	sessionID comm.SessionID, subscriptionID comm.SubscriptionID,
+) {
+	ms.lock.Lock()
+	defer ms.lock.Unlock()
+	_, ok := ms.subscribers[sessionID]
+	if !ok {
+		return
+	}
+	delete(ms.subscribers[sessionID], subscriptionID)
+}
---
+++ mpc/communication/libp2p/subscription_test.go
@@ -0,0 +1,63 @@
+package libp2p
+
+import (
+	comm "github.com/ChainSafe/chainbridge-core/mpc/communication"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type SessionSubscriptionManagerTestSuite struct {
+	suite.Suite
+}
+
+func TestRunSessionSubscriptionManagerTestSuite(t *testing.T) {
+	suite.Run(t, new(SessionSubscriptionManagerTestSuite))
+}
+
+func (s *SessionSubscriptionManagerTestSuite) SetupSuite()    {}
+func (s *SessionSubscriptionManagerTestSuite) TearDownSuite() {}
+func (s *SessionSubscriptionManagerTestSuite) SetupTest() {
+
+}
+func (s *SessionSubscriptionManagerTestSuite) TearDownTest() {}
+
+func (s *SessionSubscriptionManagerTestSuite) TestSessionSubscriptionManager_ManageSingleSubscribe_Success() {
+	subscriptionManager := NewSessionSubscriptionManager()
+
+	sChannel := make(chan *comm.WrappedMessage)
+	subscriptionID := subscriptionManager.Subscribe("1", sChannel)
+	subscribers := subscriptionManager.GetSubscribers("1")
+	s.Len(subscribers, 1)
+
+	subscriptionManager.UnSubscribe("1", subscriptionID)
+	subscribers = subscriptionManager.GetSubscribers("1")
+	s.Len(subscribers, 0)
+}
+
+func (s *SessionSubscriptionManagerTestSuite) TestSessionSubscriptionManager_ManageMultipleSubscribe_Success() {
+	subscriptionManager := NewSessionSubscriptionManager()
+
+	sub1Channel := make(chan *comm.WrappedMessage)
+	subscriptionID1 := subscriptionManager.Subscribe("1", sub1Channel)
+
+	sub2Channel := make(chan *comm.WrappedMessage)
+	_ = subscriptionManager.Subscribe("1", sub2Channel)
+
+	sub3Channel := make(chan *comm.WrappedMessage)
+	subscriptionID3 := subscriptionManager.Subscribe("2", sub3Channel)
+
+	subscribers := subscriptionManager.GetSubscribers("1")
+	s.Len(subscribers, 2)
+
+	subscribers = subscriptionManager.GetSubscribers("2")
+	s.Len(subscribers, 1)
+
+	subscriptionManager.UnSubscribe("1", subscriptionID1)
+	subscriptionManager.UnSubscribe("2", subscriptionID3)
+
+	subscribers = subscriptionManager.GetSubscribers("1")
+	s.Len(subscribers, 1)
+
+	subscribers = subscriptionManager.GetSubscribers("2")
+	s.Len(subscribers, 0)
+}
