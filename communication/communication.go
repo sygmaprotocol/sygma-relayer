2cf5138b00c36e8804e0dc266d6dd3d172ac572d
Mak Muftic <mak@chainsafe.io>
2022-04-07 13:39:55 +0200

    Change communication interface

--- communication/communication.go
+++ communication/communication.go
@@ -2,35 +2,20 @@ package communication
 
 import (
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/rs/zerolog"
 )
 
 // WrappedMessage is a message with type in it
 type WrappedMessage struct {
 	MessageType ChainBridgeMessageType `json:"message_type"`
-	SessionID   SessionID              `json:"message_id"`
+	SessionID   string                 `json:"message_id"`
 	Payload     []byte                 `json:"payload"`
 	From        peer.ID                `json:"from"`
 }
 
-//
-type SessionID string
-
-func (sid SessionID) MarshalZerologObject(e *zerolog.Event) {
-	e.Str("sessionID", string(sid))
-}
-
-//
-type SubscriptionID string
-
-func (sid SubscriptionID) MarshalZerologObject(e *zerolog.Event) {
-	e.Str("subscriptionID", string(sid))
-}
-
 // Communication
 type Communication interface {
-	Broadcast(peers peer.IDSlice, msg []byte, msgType ChainBridgeMessageType, sID SessionID)
-	ReleaseStream(sID SessionID)
-	Subscribe(msgType ChainBridgeMessageType, sID SessionID, channel chan *WrappedMessage) SubscriptionID
-	UnSubscribe(subID SubscriptionID)
+	Broadcast(peers peer.IDSlice, msg []byte, msgType ChainBridgeMessageType, sessionID string)
+	EndSession(sessionID string)
+	Subscribe(msgType ChainBridgeMessageType, sessionID string, channel chan *WrappedMessage) string
+	UnSubscribe(subscriptionID string)
 }
