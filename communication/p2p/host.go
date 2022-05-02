7039dca277bf15a06837ea283c45b7ba5c45de0f
Matija Petrunic <matija.petrunic@fer.hr>
2022-05-02 18:13:59 +0200

    Listen for connections on zero address

--- communication/p2p/host.go
+++ communication/p2p/host.go
@@ -20,7 +20,7 @@ func NewHost(privKey crypto.PrivKey, rconf relayer.MpcRelayerConfig) (host.Host,
 	}
 
 	opts := []libp2p.Option{
-		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", rconf.Port)),
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", rconf.Port)),
 		libp2p.Identity(privKey),
 		libp2p.DisableRelay(),
 		libp2p.Security(noise.ID, noise.New),
