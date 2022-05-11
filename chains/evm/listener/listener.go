4982080c35b915c0d60521e61b84f225223eb8db
Matija Petrunic <matija.petrunic@fer.hr>
2022-05-11 14:50:09 +0200

    Implement event handler for keygen, refresh and deposit

--- chains/evm/listener/listener.go
+++ chains/evm/listener/listener.go
@@ -5,41 +5,29 @@ package listener
 
 import (
 	"context"
-	"fmt"
 	"math/big"
 	"time"
 
-	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/events"
+	"github.com/ChainSafe/chainbridge-core/config/chain"
 	"github.com/ChainSafe/chainbridge-core/relayer/message"
 	"github.com/ChainSafe/chainbridge-core/store"
-	"github.com/ChainSafe/chainbridge-core/types"
-	"github.com/ethereum/go-ethereum/common"
-	ethTypes "github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/rs/zerolog/log"
 )
 
-type DepositHandler interface {
-	HandleDeposit(sourceID, destID uint8, nonce uint64, resourceID types.ResourceID, calldata, handlerResponse []byte) (*message.Message, error)
-}
-type ChainClient interface {
-	LatestBlock() (*big.Int, error)
-	CallContract(ctx context.Context, callArgs map[string]interface{}, blockNumber *big.Int) ([]byte, error)
+type EventHandler interface {
+	HandleEvent(block *big.Int, msgChan chan *message.Message) error
 }
 
-type EventListener interface {
-	FetchDeposits(ctx context.Context, address common.Address, startBlock *big.Int, endBlock *big.Int) ([]*events.Deposit, error)
-	FetchKeygenEvents(ctx context.Context, address common.Address, startBlock *big.Int, endBlock *big.Int) ([]ethTypes.Log, error)
-	FetchRefreshEvents(ctx context.Context, address common.Address, startBlock *big.Int, endBlock *big.Int) ([]ethTypes.Log, error)
+type ChainClient interface {
+	LatestBlock() (*big.Int, error)
 }
 
 type EVMListener struct {
-	client         ChainClient
-	eventListener  EventListener
-	depositHandler DepositHandler
-	bridgeAddress  common.Address
-	domainID       uint8
+	client        ChainClient
+	eventHandlers []EventHandler
 
+	domainID           uint8
 	blockstore         *store.BlockStore
 	blockRetryInterval time.Duration
 	blockConfirmations *big.Int
@@ -47,24 +35,45 @@ type EVMListener struct {
 
 // NewEVMListener creates an EVMListener that listens to deposit events on chain
 // and calls event handler when one occurs
-func NewEVMListener(client ChainClient, eventListener EventListener, depositHandler DepositHandler, bridgeAddress common.Address) *EVMListener {
-	return &EVMListener{client: client, eventListener: eventListener, depositHandler: depositHandler, bridgeAddress: bridgeAddress}
+func NewEVMListener(client ChainClient, eventHandlers []EventHandler, blockstore *store.BlockStore, config *chain.EVMConfig) *EVMListener {
+	return &EVMListener{
+		eventHandlers:      eventHandlers,
+		blockstore:         blockstore,
+		domainID:           *config.GeneralChainConfig.Id,
+		blockRetryInterval: config.BlockRetryInterval,
+		blockConfirmations: config.BlockConfirmations,
+	}
 }
 
 // ListenToEvents goes block by block of a network and executes event handlers that are
 // configured for the listener.
-func (l *EVMListener) ListenToEvents(ctx context.Context, startBlock *big.Int, msgChan chan *message.Message, errChn chan<- error) {
-	block := startBlock
+func (l *EVMListener) ListenToEvents(ctx context.Context, block *big.Int, msgChan chan *message.Message, errChn chan<- error) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			block, err := l.nextBlock(block)
+			head, err := l.client.LatestBlock()
 			if err != nil {
-				log.Err(err).Msgf("Unable to fetch next block because of: %s", err)
+				log.Error().Err(err).Msg("Unable to get latest block")
 				time.Sleep(l.blockRetryInterval)
 				continue
+			}
+			if block == nil {
+				block = head
+			}
+			// Sleep if the difference is less than needed block confirmations; (latest - current) < BlockDelay
+			if big.NewInt(0).Sub(head, block).Cmp(l.blockConfirmations) == -1 {
+				time.Sleep(l.blockRetryInterval)
+				continue
+			}
+
+			for _, handler := range l.eventHandlers {
+				err := handler.HandleEvent(block, msgChan)
+				if err != nil {
+					log.Error().Err(err).Str("DomainID", string(l.domainID)).Msgf("Unable to handle events")
+					continue
+				}
 			}
 
 			//Write to block store. Not a critical operation, no need to retry
@@ -72,40 +81,7 @@ func (l *EVMListener) ListenToEvents(ctx context.Context, startBlock *big.Int, m
 			if err != nil {
 				log.Error().Str("block", block.String()).Err(err).Msg("Failed to write latest block to blockstore")
 			}
+			block.Add(block, big.NewInt(1))
 		}
 	}
 }
-
-func (l *EVMListener) nextBlock(previousBlock *big.Int) (*big.Int, error) {
-	head, err := l.client.LatestBlock()
-	if err != nil {
-		return nil, err
-	}
-	if previousBlock == nil {
-		return head, nil
-	}
-
-	nextBlock := previousBlock.Add(previousBlock, big.NewInt(1))
-	if big.NewInt(0).Sub(head, nextBlock).Cmp(l.blockConfirmations) == -1 {
-		return nil, fmt.Errorf("block %s difference from head %s less than configured block confirmations %s", nextBlock, head, l.blockConfirmations)
-	}
-
-	return nextBlock, nil
-}
-
-/*
-func (l *EVMListener) handleDeposits(deposits []*events.Deposit, block *big.Int, domainID uint8, ch chan *message.Message) {
-	for _, d := range deposits {
-		log.Debug().Msgf("Deposit log found from sender: %s in block: %s with  destinationDomainId: %v, resourceID: %s, depositNonce: %v", d.SenderAddress, block.String(), d.DestinationDomainID, d.ResourceID, d.DepositNonce)
-
-		m, err := l.eventHandler.HandleEvent(domainID, d.DestinationDomainID, d.DepositNonce, d.ResourceID, d.Data, d.HandlerResponse)
-		if err != nil {
-			log.Error().Str("block", block.String()).Uint8("domainID", domainID).Msgf("%v", err)
-			continueblock delay
-		}
-
-		log.Debug().Msgf("Resolved message %+v in block %s", m, block.String())
-		ch <- m
-	}
-}
-*/
