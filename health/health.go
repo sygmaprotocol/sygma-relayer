521c83dedfa3350dcbea06c4a5b5be5a682f1f19
Matija Petrunic <matija.petrunic@fer.hr>
2022-06-08 14:04:01 +0200

    Add health check to app

--- health/health.go
+++ health/health.go
@@ -2,15 +2,16 @@ package health
 
 import (
 	"fmt"
-	"github.com/rs/zerolog/log"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 // StartHealthEndpoint starts /health endpoint on provided port that returns ok on invocation
-func StartHealthEndpoint(port string) {
+func StartHealthEndpoint(port uint16) {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte("ok"))
 	})
-	_ = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	_ = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	log.Info().Msgf("started /health endpoint on port %s", port)
 }
