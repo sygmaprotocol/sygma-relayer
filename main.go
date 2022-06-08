c49c5beef9e834e0ef90511b2dd3464a567d4a3b
Matija Petrunic <matija.petrunic@fer.hr>
2022-06-08 11:43:24 +0200

    Add app entrypoint

---
+++ main.go
@@ -0,0 +1,10 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package main
+
+import "github.com/ChainSafe/chainbridge-core/app"
+
+func main() {
+	app.Execute()
+}
