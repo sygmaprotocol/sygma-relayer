c6c78c85801e321cd35847aca855ff11df1f4867
Mak Muftic <mak@chainsafe.io>
2022-06-22 13:36:11 +0200

    Additional logging

--- app/cmd.go
+++ app/cmd.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"os"
 
 	"github.com/ChainSafe/chainbridge-core/chains/evm/cli/deploy"
 	"github.com/ChainSafe/chainbridge-core/flags"
@@ -30,7 +31,9 @@ func init() {
 }
 
 func Execute() {
+	log.Info().Msgf("Preparing CLI commands, args %+v", os.Args)
 	rootCMD.AddCommand(runCMD, deploy.DeployEVM)
+	log.Info().Msg("CLI commands ready for execution")
 	if err := rootCMD.Execute(); err != nil {
 		log.Fatal().Err(err).Msg("failed to execute root cmd")
 	}
