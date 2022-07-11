13ac07485faae9e2a10a86b6a5f08c248020c7c9
Matija Petrunic <matija.petrunic@fer.hr>
2022-07-11 14:20:43 +0200

    Bump contracts

---
+++ chains/evm/calls/contracts/accessControlSegregator/accessControlSegregator.go
@@ -0,0 +1,28 @@
+package accessControlSegregator
+
+import (
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/contracts"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/transactor"
+
+	"github.com/ChainSafe/chainbridge-hub/chains/evm/calls/consts"
+)
+
+type AccessControlSegregatorContract struct {
+	contracts.Contract
+}
+
+func NewAccessControlSegregatorContract(
+	client calls.ContractCallerDispatcher,
+	address common.Address,
+	transactor transactor.Transactor,
+) *AccessControlSegregatorContract {
+	a, _ := abi.JSON(strings.NewReader(consts.AccessControlSegregatorABI))
+	b := common.FromHex(consts.AccessControlSegregatorBin)
+	return &AccessControlSegregatorContract{contracts.NewContract(address, a, b, client, transactor)}
+}
