92009f1a4ca4c5592fbccfd5daa8dfa60b93dcfc
tcar <tcar121293@gmail.com>
2022-11-09 13:46:45 +0100

    move config to substrate package and add tests;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- chains/substrate/config.go
+++ chains/substrate/config.go
@@ -1,9 +1,10 @@
 // The Licensed Work is (c) 2022 Sygma
 // SPDX-License-Identifier: BUSL-1.1
 
-package config
+package substrate
 
 import (
+	"fmt"
 	"math/big"
 	"time"
 
@@ -13,15 +14,31 @@ import (
 	"github.com/ChainSafe/chainbridge-core/config/chain"
 )
 
+type RawSubstrateConfig struct {
+	chain.GeneralChainConfig `mapstructure:",squash"`
+	StartBlock               int64  `mapstructure:"startBlock"`
+	BlockConfirmations       int64  `mapstructure:"blockConfirmations" default:"10"`
+	BlockInterval            int64  `mapstructure:"blockInterval" default:"5"`
+	BlockRetryInterval       uint64 `mapstructure:"blockRetryInterval" default:"5"`
+}
+
 type SubstrateConfig struct {
 	GeneralChainConfig chain.GeneralChainConfig
-	MaxGasPrice        *big.Int
-	GasMultiplier      *big.Float
-	GasLimit           *big.Int
 	StartBlock         *big.Int
 	BlockConfirmations *big.Int
 	BlockInterval      *big.Int
 	BlockRetryInterval time.Duration
+}
+
+func (c *RawSubstrateConfig) Validate() error {
+	if err := c.GeneralChainConfig.Validate(); err != nil {
+		return err
+	}
+
+	if c.BlockConfirmations != 0 && c.BlockConfirmations < 1 {
+		return fmt.Errorf("blockConfirmations has to be >=1")
+	}
+	return nil
 }
 
 // NewSubstrateConfig decodes and validates an instance of an SubstrateConfig from
@@ -47,24 +64,10 @@ func NewSubstrateConfig(chainConfig map[string]interface{}) (*SubstrateConfig, e
 	config := &SubstrateConfig{
 		GeneralChainConfig: c.GeneralChainConfig,
 		BlockRetryInterval: time.Duration(c.BlockRetryInterval) * time.Second,
-		GasLimit:           big.NewInt(c.GasLimit),
-		MaxGasPrice:        big.NewInt(c.MaxGasPrice),
-		GasMultiplier:      big.NewFloat(c.GasMultiplier),
 		StartBlock:         big.NewInt(c.StartBlock),
 		BlockConfirmations: big.NewInt(c.BlockConfirmations),
 		BlockInterval:      big.NewInt(c.BlockInterval),
 	}
 
 	return config, nil
-}
-
-type RawSubstrateConfig struct {
-	chain.GeneralChainConfig `mapstructure:",squash"`
-	MaxGasPrice              int64   `mapstructure:"maxGasPrice" default:"20000000000"`
-	GasMultiplier            float64 `mapstructure:"gasMultiplier" default:"1"`
-	GasLimit                 int64   `mapstructure:"gasLimit" default:"2000000"`
-	StartBlock               int64   `mapstructure:"startBlock"`
-	BlockConfirmations       int64   `mapstructure:"blockConfirmations" default:"10"`
-	BlockInterval            int64   `mapstructure:"blockInterval" default:"5"`
-	BlockRetryInterval       uint64  `mapstructure:"blockRetryInterval" default:"5"`
 }
