f2c40c012a57cec744901263c1f95c14210026d4
tcar121293 <tcar121293@gmail.com>
2022-11-10 10:09:38 +0100

    feat: Add substrate chain type (#42)
    Add substrate chain type

---
+++ chains/substrate/config_test.go
@@ -0,0 +1,103 @@
+package substrate
+
+import (
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/ChainSafe/chainbridge-core/config/chain"
+	"github.com/stretchr/testify/suite"
+)
+
+type NewSubstrateConfigTestSuite struct {
+	suite.Suite
+}
+
+func TestRunNewSubstrateConfigTestSuite(t *testing.T) {
+	suite.Run(t, new(NewSubstrateConfigTestSuite))
+}
+
+func (s *NewSubstrateConfigTestSuite) SetupSuite()    {}
+func (s *NewSubstrateConfigTestSuite) TearDownSuite() {}
+func (s *NewSubstrateConfigTestSuite) SetupTest()     {}
+func (s *NewSubstrateConfigTestSuite) TearDownTest()  {}
+
+func (s *NewSubstrateConfigTestSuite) Test_FailedDecode() {
+	_, err := NewSubstrateConfig(map[string]interface{}{
+		"startBlock": "invalid",
+	})
+
+	s.NotNil(err)
+}
+
+func (s *NewSubstrateConfigTestSuite) Test_FailedGeneralConfigValidation() {
+	_, err := NewSubstrateConfig(map[string]interface{}{})
+
+	s.NotNil(err)
+}
+
+func (s *NewSubstrateConfigTestSuite) Test_InvalidBlockConfirmation() {
+	_, err := NewSubstrateConfig(map[string]interface{}{
+		"id":                 1,
+		"endpoint":           "ws://domain.com",
+		"name":               "substrate1",
+		"blockConfirmations": -1,
+	})
+
+	s.NotNil(err)
+	s.Equal(err.Error(), "blockConfirmations has to be >=1")
+}
+
+func (s *NewSubstrateConfigTestSuite) Test_ValidConfig() {
+	rawConfig := map[string]interface{}{
+		"id":       1,
+		"endpoint": "ws://domain.com",
+		"name":     "substrate1",
+	}
+
+	actualConfig, err := NewSubstrateConfig(rawConfig)
+
+	id := new(uint8)
+	*id = 1
+	s.Nil(err)
+	s.Equal(*actualConfig, SubstrateConfig{
+		GeneralChainConfig: chain.GeneralChainConfig{
+			Name:     "substrate1",
+			Endpoint: "ws://domain.com",
+			Id:       id,
+		},
+		StartBlock:         big.NewInt(0),
+		BlockConfirmations: big.NewInt(10),
+		BlockInterval:      big.NewInt(5),
+		BlockRetryInterval: time.Duration(5) * time.Second,
+	})
+}
+
+func (s *NewSubstrateConfigTestSuite) Test_ValidConfigWithCustomParams() {
+	rawConfig := map[string]interface{}{
+		"id":                 1,
+		"endpoint":           "ws://domain.com",
+		"name":               "substrate1",
+		"startBlock":         1000,
+		"blockConfirmations": 10,
+		"blockRetryInterval": 10,
+		"blockInterval":      2,
+	}
+
+	actualConfig, err := NewSubstrateConfig(rawConfig)
+
+	id := new(uint8)
+	*id = 1
+	s.Nil(err)
+	s.Equal(*actualConfig, SubstrateConfig{
+		GeneralChainConfig: chain.GeneralChainConfig{
+			Name:     "substrate1",
+			Endpoint: "ws://domain.com",
+			Id:       id,
+		},
+		StartBlock:         big.NewInt(1000),
+		BlockConfirmations: big.NewInt(10),
+		BlockInterval:      big.NewInt(2),
+		BlockRetryInterval: time.Duration(10) * time.Second,
+	})
+}
