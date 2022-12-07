a646bfa36f87c3651e56bb4aa9d687a23d157e26
Matija Petrunic <matija.petrunic@gmail.com>
2022-12-07 16:54:25 +0100

    Fix deposit data construction

--- chains/evm/calls/contracts/bridge/deposit.go
+++ chains/evm/calls/contracts/bridge/deposit.go
@@ -4,18 +4,17 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
 )
 
 func ConstructPermissionlessGenericDepositData(metadata []byte, executionFunctionSig []byte, executeContractAddress []byte, metadataDepositor []byte, maxFee *big.Int) []byte {
 	var data []byte
-	data = append(data, math.PaddedBigBytes(maxFee, 32)...)
+	data = append(data, common.LeftPadBytes(maxFee.Bytes(), 32)...)
 	data = append(data, common.LeftPadBytes(big.NewInt(int64(len(executionFunctionSig))).Bytes(), 2)...)
 	data = append(data, executionFunctionSig...)
-	data = append(data, []byte{byte(len(executeContractAddress))}...)
+	data = append(data, byte(len(executeContractAddress)))
 	data = append(data, executeContractAddress...)
-	data = append(data, []byte{byte(len(metadataDepositor))}...)
-	data = append(data, metadataDepositor...)
+	data = append(data, byte(32))
+	data = append(data, common.LeftPadBytes(metadataDepositor, 32)...)
 	data = append(data, metadata...)
 	return data
 }
