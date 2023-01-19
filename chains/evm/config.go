4e2b10be3fbdf067652bdbb1f31a5e47f0242062
Matija Petrunic <matija.petrunic@gmail.com>
2023-01-19 16:19:16 +0100

    Add custom EVM config

---
+++ chains/evm/config.go
@@ -0,0 +1,92 @@
+package evm
+
+import (
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/creasty/defaults"
+	"github.com/mitchellh/mapstructure"
+
+	"github.com/ChainSafe/chainbridge-core/config/chain"
+)
+
+type HandlerConfig struct {
+	Address string
+	Type    string
+}
+
+type EVMConfig struct {
+	GeneralChainConfig chain.GeneralChainConfig
+	Bridge             string
+	Handlers           []HandlerConfig
+	MaxGasPrice        *big.Int
+	GasMultiplier      *big.Float
+	GasLimit           *big.Int
+	StartBlock         *big.Int
+	BlockConfirmations *big.Int
+	BlockInterval      *big.Int
+	BlockRetryInterval time.Duration
+}
+
+type RawEVMConfig struct {
+	chain.GeneralChainConfig `mapstructure:",squash"`
+	Bridge                   string          `mapstructure:"bridge"`
+	Handlers                 []HandlerConfig `mapstrcture:"handlers"`
+	MaxGasPrice              int64           `mapstructure:"maxGasPrice" default:"20000000000"`
+	GasMultiplier            float64         `mapstructure:"gasMultiplier" default:"1"`
+	GasLimit                 int64           `mapstructure:"gasLimit" default:"2000000"`
+	StartBlock               int64           `mapstructure:"startBlock"`
+	BlockConfirmations       int64           `mapstructure:"blockConfirmations" default:"10"`
+	BlockInterval            int64           `mapstructure:"blockInterval" default:"5"`
+	BlockRetryInterval       uint64          `mapstructure:"blockRetryInterval" default:"5"`
+}
+
+func (c *RawEVMConfig) Validate() error {
+	if err := c.GeneralChainConfig.Validate(); err != nil {
+		return err
+	}
+	if c.Bridge == "" {
+		return fmt.Errorf("required field chain.Bridge empty for chain %v", *c.Id)
+	}
+	if c.BlockConfirmations != 0 && c.BlockConfirmations < 1 {
+		return fmt.Errorf("blockConfirmations has to be >=1")
+	}
+	return nil
+}
+
+// NewEVMConfig decodes and validates an instance of an EVMConfig from
+// raw chain config
+func NewEVMConfig(chainConfig map[string]interface{}) (*EVMConfig, error) {
+	var c RawEVMConfig
+	err := mapstructure.Decode(chainConfig, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	err = defaults.Set(&c)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	c.GeneralChainConfig.ParseFlags()
+	config := &EVMConfig{
+		GeneralChainConfig: c.GeneralChainConfig,
+		Handlers:           c.Handlers,
+		Bridge:             c.Bridge,
+		BlockRetryInterval: time.Duration(c.BlockRetryInterval) * time.Second,
+		GasLimit:           big.NewInt(c.GasLimit),
+		MaxGasPrice:        big.NewInt(c.MaxGasPrice),
+		GasMultiplier:      big.NewFloat(c.GasMultiplier),
+		StartBlock:         big.NewInt(c.StartBlock),
+		BlockConfirmations: big.NewInt(c.BlockConfirmations),
+		BlockInterval:      big.NewInt(c.BlockInterval),
+	}
+
+	return config, nil
+}
---
+++ chains/evm/config_test.go
@@ -0,0 +1,148 @@
+package evm_test
+
+import (
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/ChainSafe/chainbridge-core/config/chain"
+	"github.com/ChainSafe/sygma-relayer/chains/evm"
+	"github.com/stretchr/testify/suite"
+)
+
+type NewEVMConfigTestSuite struct {
+	suite.Suite
+}
+
+func TestRunNewEVMConfigTestSuite(t *testing.T) {
+	suite.Run(t, new(NewEVMConfigTestSuite))
+}
+
+func (s *NewEVMConfigTestSuite) Test_FailedDecode() {
+	_, err := evm.NewEVMConfig(map[string]interface{}{
+		"gasLimit": "invalid",
+	})
+
+	s.NotNil(err)
+}
+
+func (s *NewEVMConfigTestSuite) Test_FailedGeneralConfigValidation() {
+	_, err := evm.NewEVMConfig(map[string]interface{}{})
+
+	s.NotNil(err)
+}
+
+func (s *NewEVMConfigTestSuite) Test_FailedEVMConfigValidation() {
+	_, err := evm.NewEVMConfig(map[string]interface{}{
+		"id":       1,
+		"endpoint": "ws://domain.com",
+		"name":     "evm1",
+		"from":     "address",
+	})
+
+	s.NotNil(err)
+}
+
+func (s *NewEVMConfigTestSuite) Test_InvalidBlockConfirmation() {
+	_, err := evm.NewEVMConfig(map[string]interface{}{
+		"id":                 1,
+		"endpoint":           "ws://domain.com",
+		"name":               "evm1",
+		"from":               "address",
+		"bridge":             "bridgeAddress",
+		"blockConfirmations": -1,
+	})
+
+	s.NotNil(err)
+	s.Equal(err.Error(), "blockConfirmations has to be >=1")
+}
+
+func (s *NewEVMConfigTestSuite) Test_ValidConfig() {
+	rawConfig := map[string]interface{}{
+		"id":       1,
+		"endpoint": "ws://domain.com",
+		"name":     "evm1",
+		"from":     "address",
+		"bridge":   "bridgeAddress",
+	}
+
+	actualConfig, err := evm.NewEVMConfig(rawConfig)
+
+	id := new(uint8)
+	*id = 1
+	s.Nil(err)
+	s.Equal(*actualConfig, evm.EVMConfig{
+		GeneralChainConfig: chain.GeneralChainConfig{
+			Name:     "evm1",
+			Endpoint: "ws://domain.com",
+			Id:       id,
+		},
+		Bridge:             "bridgeAddress",
+		Handlers:           []evm.HandlerConfig{},
+		GasLimit:           big.NewInt(2000000),
+		MaxGasPrice:        big.NewInt(20000000000),
+		GasMultiplier:      big.NewFloat(1),
+		StartBlock:         big.NewInt(0),
+		BlockConfirmations: big.NewInt(10),
+		BlockInterval:      big.NewInt(5),
+		BlockRetryInterval: time.Duration(5) * time.Second,
+	})
+}
+
+func (s *NewEVMConfigTestSuite) Test_ValidConfigWithCustomTxParams() {
+	rawConfig := map[string]interface{}{
+		"id":       1,
+		"endpoint": "ws://domain.com",
+		"name":     "evm1",
+		"from":     "address",
+		"bridge":   "bridgeAddress",
+		"handlers": []evm.HandlerConfig{
+			{
+				Type:    "erc20",
+				Address: "address1",
+			},
+			{
+				Type:    "erc721",
+				Address: "address2",
+			},
+		},
+		"maxGasPrice":        1000,
+		"gasMultiplier":      1000,
+		"gasLimit":           1000,
+		"startBlock":         1000,
+		"blockConfirmations": 10,
+		"blockRetryInterval": 10,
+		"blockInterval":      2,
+	}
+
+	actualConfig, err := evm.NewEVMConfig(rawConfig)
+
+	id := new(uint8)
+	*id = 1
+	s.Nil(err)
+	s.Equal(*actualConfig, evm.EVMConfig{
+		GeneralChainConfig: chain.GeneralChainConfig{
+			Name:     "evm1",
+			Endpoint: "ws://domain.com",
+			Id:       id,
+		},
+		Bridge: "bridgeAddress",
+		Handlers: []evm.HandlerConfig{
+			{
+				Type:    "erc20",
+				Address: "address1",
+			},
+			{
+				Type:    "erc721",
+				Address: "address2",
+			},
+		},
+		GasLimit:           big.NewInt(1000),
+		MaxGasPrice:        big.NewInt(1000),
+		GasMultiplier:      big.NewFloat(1000),
+		StartBlock:         big.NewInt(1000),
+		BlockConfirmations: big.NewInt(10),
+		BlockInterval:      big.NewInt(2),
+		BlockRetryInterval: time.Duration(10) * time.Second,
+	})
+}
