6c372af3b3d50538978a4928d09c2a69fc74f9ce
Matija Petrunic <matija.petrunic@gmail.com>
2023-01-30 13:36:40 +0100

    Execute health check when health endpoint called

--- app/app.go
+++ app/app.go
@@ -13,8 +13,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/ChainSafe/sygma-relayer/comm"
-
 	coreEvm "github.com/ChainSafe/chainbridge-core/chains/evm"
 	coreEvents "github.com/ChainSafe/chainbridge-core/chains/evm/calls/events"
 	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/evmclient"
@@ -72,8 +70,6 @@ func Run() error {
 
 	log.Info().Msg("Successfully loaded configuration")
 
-	go health.StartHealthEndpoint(configuration.RelayerConfig.HealthPort)
-
 	topologyProvider, err := topology.NewNetworkTopologyProvider(configuration.RelayerConfig.MpcConfig.TopologyConfiguration)
 	panicOnError(err)
 	topologyStore := topology.NewTopologyStore(configuration.RelayerConfig.MpcConfig.TopologyConfiguration.Path)
@@ -117,7 +113,7 @@ func Run() error {
 	log.Info().Str("peerID", host.ID().String()).Msg("Successfully created libp2p host")
 
 	healthComm := p2p.NewCommunication(host, "p2p/health")
-	go comm.ExecuteCommHealthCheck(healthComm, host.Peerstore().Peers())
+	go health.StartHealthEndpoint(configuration.RelayerConfig.HealthPort, healthComm, host)
 
 	communication := p2p.NewCommunication(host, "p2p/sygma")
 	electorFactory := elector.NewCoordinatorElectorFactory(host, configuration.RelayerConfig.BullyConfig)
--- health/health.go
+++ health/health.go
@@ -7,12 +7,16 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/ChainSafe/sygma-relayer/comm"
+	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/rs/zerolog/log"
 )
 
 // StartHealthEndpoint starts /health endpoint on provided port that returns ok on invocation
-func StartHealthEndpoint(port uint16) {
+func StartHealthEndpoint(port uint16, c comm.Communication, h host.Host) {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		log.Debug().Msg("Health endpoint called")
+		go comm.ExecuteCommHealthCheck(c, h.Peerstore().Peers())
 		_, _ = w.Write([]byte("ok"))
 	})
 	_ = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
