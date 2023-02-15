2c141eb73bcd847870f991a08d8d2b7524086cf3
Matija Petrunic <matija.petrunic@gmail.com>
2023-02-15 16:52:06 +0100

    Update handlers test

--- chains/substrate/events/handlers_test.go
+++ chains/substrate/events/handlers_test.go
@@ -40,10 +40,10 @@ func (s *SystemUpdateHandlerTestSuite) Test_UpdateMetadataFails() {
 	evtsRec := substrate_types.EventRecords{
 		System_CodeUpdated: make([]substrate_types.EventSystemCodeUpdated, 1),
 	}
-	evts := &events.Events{
+	evts := []*events.Events{{
 		evtsRec,
 		[]events.Deposit{},
-	}
+	}}
 	msgChan := make(chan []*message.Message, 1)
 	err := s.systemUpdateHandler.HandleEvents(evts, msgChan)
 
@@ -51,8 +51,17 @@ func (s *SystemUpdateHandlerTestSuite) Test_UpdateMetadataFails() {
 	s.Equal(len(msgChan), 0)
 }
 
-func (s *SystemUpdateHandlerTestSuite) Test_NoMetadataUpdate() {
-	evts := events.Events{}
+func (s *SystemUpdateHandlerTestSuite) Test_NoEvents() {
+	evts := []*events.Events{}
+	msgChan := make(chan []*message.Message, 1)
+	err := s.systemUpdateHandler.HandleEvents(evts, msgChan)
+
+	s.Nil(err)
+	s.Equal(len(msgChan), 0)
+}
+
+func (s *SystemUpdateHandlerTestSuite) Test_NoMetadatUpdate() {
+	evts := []*events.Events{{}}
 	msgChan := make(chan []*message.Message, 1)
 	err := s.systemUpdateHandler.HandleEvents(evts, msgChan)
 
@@ -66,8 +75,11 @@ func (s *SystemUpdateHandlerTestSuite) Test_SuccesfullMetadataUpdate() {
 	evtsRec := substrate_types.EventRecords{
 		System_CodeUpdated: make([]substrate_types.EventSystemCodeUpdated, 1),
 	}
-	evts := events.Events{evtsRec,
-		[]events.Deposit{},
+	evts := []*events.Events{
+		{},
+		{
+			EventRecords: evtsRec,
+		},
 	}
 	msgChan := make(chan []*message.Message, 1)
 	err := s.systemUpdateHandler.HandleEvents(evts, msgChan)
@@ -138,9 +150,12 @@ func (s *DepositHandlerTestSuite) Test_HandleDepositFails_ExecutionContinue() {
 	evtsRec := substrate_types.EventRecords{
 		System_CodeUpdated: make([]substrate_types.EventSystemCodeUpdated, 1),
 	}
-	evts := events.Events{evtsRec,
-		[]events.Deposit{
-			*d1, *d2,
+	evts := []*events.Events{
+		{
+			evtsRec,
+			[]events.Deposit{
+				*d1, *d2,
+			},
 		},
 	}
 	err := s.depositEventHandler.HandleEvents(evts, msgChan)
@@ -196,9 +211,12 @@ func (s *DepositHandlerTestSuite) Test_SuccessfulHandleDeposit() {
 	evtsRec := substrate_types.EventRecords{
 		System_CodeUpdated: make([]substrate_types.EventSystemCodeUpdated, 1),
 	}
-	evts := events.Events{evtsRec,
-		[]events.Deposit{
-			*d1, *d2,
+	evts := []*events.Events{
+		{
+			evtsRec,
+			[]events.Deposit{
+				*d1, *d2,
+			},
 		},
 	}
 	err := s.depositEventHandler.HandleEvents(evts, msgChan)
@@ -253,9 +271,12 @@ func (s *DepositHandlerTestSuite) Test_HandleDepositPanics_ExecutionContinues() 
 	evtsRec := substrate_types.EventRecords{
 		System_CodeUpdated: make([]substrate_types.EventSystemCodeUpdated, 1),
 	}
-	evts := events.Events{evtsRec,
-		[]events.Deposit{
-			*d1, *d2,
+	evts := []*events.Events{
+		{
+			evtsRec,
+			[]events.Deposit{
+				*d1, *d2,
+			},
 		},
 	}
 	err := s.depositEventHandler.HandleEvents(evts, msgChan)
@@ -263,4 +284,95 @@ func (s *DepositHandlerTestSuite) Test_HandleDepositPanics_ExecutionContinues() 
 
 	s.Nil(err)
 	s.Equal(msgs, []*message.Message{{DepositNonce: 2}})
+}
+
+func (s *DepositHandlerTestSuite) Test_SuccessfulHandleDeposit_MultipleBlocks() {
+	d1 := &events.Deposit{
+		DepositNonce:        1,
+		DestinationDomainID: 2,
+		ResourceID:          types.ResourceID{},
+		DepositType:         message.FungibleTransfer,
+		HandlerResponse:     []byte{},
+		Data:                []byte{},
+	}
+	d2 := &events.Deposit{
+		DepositNonce:        2,
+		DestinationDomainID: 2,
+		ResourceID:          types.ResourceID{},
+		DepositType:         message.FungibleTransfer,
+		HandlerResponse:     []byte{},
+		Data:                []byte{},
+	}
+	d3 := &events.Deposit{
+		DepositNonce:        3,
+		DestinationDomainID: 3,
+		ResourceID:          types.ResourceID{},
+		DepositType:         message.FungibleTransfer,
+		HandlerResponse:     []byte{},
+		Data:                []byte{},
+	}
+	s.mockDepositHandler.EXPECT().HandleDeposit(
+		s.domainID,
+		d1.DestinationDomainID,
+		d1.DepositNonce,
+		d1.ResourceID,
+		d1.Data,
+		d1.DepositType,
+		d1.HandlerResponse,
+	).Return(
+		&message.Message{DepositNonce: 1},
+		nil,
+	)
+	s.mockDepositHandler.EXPECT().HandleDeposit(
+		s.domainID,
+		d2.DestinationDomainID,
+		d2.DepositNonce,
+		d2.ResourceID,
+		d2.Data,
+		d2.DepositType,
+		d2.HandlerResponse,
+	).Return(
+		&message.Message{DepositNonce: 2},
+		nil,
+	)
+	s.mockDepositHandler.EXPECT().HandleDeposit(
+		s.domainID,
+		d3.DestinationDomainID,
+		d3.DepositNonce,
+		d3.ResourceID,
+		d3.Data,
+		d3.DepositType,
+		d3.HandlerResponse,
+	).Return(
+		&message.Message{DepositNonce: 3},
+		nil,
+	)
+
+	msgChan := make(chan []*message.Message, 3)
+	evtsRec := substrate_types.EventRecords{
+		System_CodeUpdated: make([]substrate_types.EventSystemCodeUpdated, 1),
+	}
+	evts := []*events.Events{
+		{
+			evtsRec,
+			[]events.Deposit{
+				*d1, *d2,
+			},
+		},
+		{
+			evtsRec,
+			[]events.Deposit{},
+		},
+		{
+			evtsRec,
+			[]events.Deposit{
+				*d3,
+			},
+		},
+	}
+	err := s.depositEventHandler.HandleEvents(evts, msgChan)
+	msgs := <-msgChan
+
+	s.Nil(err)
+	s.Equal(msgs, []*message.Message{{DepositNonce: 1}, {DepositNonce: 2}, {DepositNonce: 3}})
 }
