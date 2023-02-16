c675e9debf038b531d30e207c8544a0f4ada2df7
Mak Muftic <mak@chainsafe.io>
2023-02-16 15:00:28 +0100

    add comment for util function

--- chains/util.go
+++ chains/util.go
@@ -2,6 +2,7 @@ package chains
 
 import "math/big"
 
+// CalculateStartingBlock returns first block number (smaller or equal) that is dividable with block confirmations
 func CalculateStartingBlock(startBlock *big.Int, blockConfirmations *big.Int) *big.Int {
 	mod := big.NewInt(0).Mod(startBlock, blockConfirmations)
 	// startBlock % blockConfirmations == 0
