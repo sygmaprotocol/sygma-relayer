e8eb6a813d97622adbc7f2308c26cf9a73ae0723
Mak Muftic <mak@chainsafe.io>
2023-02-17 15:21:37 +0100

    remove uncessary if

--- chains/util.go
+++ chains/util.go
@@ -5,9 +5,6 @@ import "math/big"
 // CalculateStartingBlock returns first block number (smaller or equal) that is dividable with block confirmations
 func CalculateStartingBlock(startBlock *big.Int, blockConfirmations *big.Int) *big.Int {
 	mod := big.NewInt(0).Mod(startBlock, blockConfirmations)
-	// startBlock % blockConfirmations == 0
-	if mod.Cmp(big.NewInt(0)) != 0 {
-		startBlock.Sub(startBlock, mod)
-	}
+	startBlock.Sub(startBlock, mod)
 	return startBlock
 }
