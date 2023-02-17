72907dc1c2f2c2c922ac7042c57a1bf26b3e0ccd
mace <mak@chainsafe.io>
2023-02-17 15:31:21 +0100

    fix: starting block with latest flag (#104)

--- chains/evm/chain.go
+++ chains/evm/chain.go
@@ -4,9 +4,16 @@
 package evm
 
 import (
+	"context"
+	"fmt"
 	"github.com/ChainSafe/chainbridge-core/chains/evm"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/evmclient"
 	"github.com/ChainSafe/chainbridge-core/relayer/message"
+	"github.com/ChainSafe/chainbridge-core/store"
+	"github.com/ChainSafe/sygma-relayer/chains"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"math/big"
 )
 
 type BatchProposalExecutor interface {
@@ -14,20 +21,74 @@ type BatchProposalExecutor interface {
 }
 
 type EVMChain struct {
-	evm.EVMChain
-	executor BatchProposalExecutor
+	client        *evmclient.EVMClient
+	listener      evm.EventListener
+	executor      BatchProposalExecutor
+	blockstore    *store.BlockStore
+	domainID      uint8
+	startBlock    *big.Int
+	blockInterval *big.Int
+	freshStart    bool
+	latestBlock   bool
+	logger        zerolog.Logger
 }
 
-func NewEVMChain(evmChain evm.EVMChain, executor BatchProposalExecutor) *EVMChain {
+func NewEVMChain(
+	client *evmclient.EVMClient, listener evm.EventListener, executor BatchProposalExecutor,
+	blockstore *store.BlockStore, domainID uint8, startBlock *big.Int, blockInterval *big.Int,
+	freshStart bool, latestBlock bool,
+) *EVMChain {
 	return &EVMChain{
-		EVMChain: evmChain,
-		executor: executor,
+		client:        client,
+		listener:      listener,
+		executor:      executor,
+		blockstore:    blockstore,
+		domainID:      domainID,
+		startBlock:    startBlock,
+		blockInterval: blockInterval,
+		freshStart:    freshStart,
+		latestBlock:   latestBlock,
+		logger:        log.With().Str("domain", string(domainID)).Logger(),
 	}
 }
 
 func (c *EVMChain) Write(msgs []*message.Message) {
 	err := c.executor.Execute(msgs)
 	if err != nil {
-		log.Err(err).Msgf("error writing messages %+v on network %d", msgs, c.DomainID())
+		log.Err(err).Str("domain", string(c.domainID)).Msgf("error writing messages %+v", msgs)
 	}
+}
+
+func (c *EVMChain) PollEvents(ctx context.Context, sysErr chan<- error, msgChan chan []*message.Message) {
+	c.logger.Info().Msg("Polling Blocks...")
+
+	startBlock, err := c.blockstore.GetStartBlock(
+		c.domainID,
+		c.startBlock,
+		c.latestBlock,
+		c.freshStart,
+	)
+	if err != nil {
+		sysErr <- fmt.Errorf("error %w on getting last stored block for domain %d", err, c.domainID)
+		return
+	}
+
+	// start from latest
+	if startBlock == nil {
+		head, err := c.client.LatestBlock()
+		if err != nil {
+			sysErr <- fmt.Errorf("error %w on getting latest block for domain %d", err, c.domainID)
+			return
+		}
+		startBlock = head
+	}
+
+	startBlock = chains.CalculateStartingBlock(startBlock, c.blockInterval)
+	c.logger.Info().Msgf("Starting block: %s", startBlock.String())
+
+	go c.listener.ListenToEvents(ctx, startBlock, msgChan, sysErr)
+}
+
+func (c *EVMChain) DomainID() uint8 {
+	return c.domainID
 }
