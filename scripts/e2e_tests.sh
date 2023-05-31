1980918290a91d77102eda1600d369754c31a33d
mace <mak@chainsafe.io>
2023-05-31 14:37:55 +0200

    chore: update license (#187)
    Co-authored-by: MakMuftic <makmuftic@chainsafe.io>

--- scripts/e2e_tests.sh
+++ scripts/e2e_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # The Licensed Work is (c) 2022 Sygma
-# SPDX-License-Identifier: BUSL-1.1
+# SPDX-License-Identifier: LGPL-3.0-only
 
 go test -timeout 30m -p=1 $(go list ./... | grep 'e2e')
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # The Licensed Work is (c) 2022 Sygma
-# SPDX-License-Identifier: BUSL-1.1
+# SPDX-License-Identifier: LGPL-3.0-only
 
 
 CVPKG=$(go list ./... | grep -v 'e2e\|generated\|bindata\|mock\|main.go\|' | tr '\n' ',')
