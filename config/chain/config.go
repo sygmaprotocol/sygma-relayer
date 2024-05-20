0b422be155f54d846b7722179909d6119955e46f
MakMuftic <mak@chainsafe.io>
2024-05-20 16:47:16 +0200

    update property to sygma id

--- config/chain/config.go
+++ config/chain/config.go
@@ -9,7 +9,7 @@ import (
 
 type GeneralChainConfig struct {
 	Name           string `mapstructure:"name"`
-	Id             *uint8 `mapstructure:"id"`
+	Id             *uint8 `mapstructure:"sygmaId"`
 	Endpoint       string `mapstructure:"endpoint"`
 	Type           string `mapstructure:"type"`
 	BlockstorePath string `mapstructure:"blockstorePath"`
@@ -22,7 +22,7 @@ type GeneralChainConfig struct {
 func (c *GeneralChainConfig) Validate() error {
 	// viper defaults to 0 for not specified ints
 	if c.Id == nil {
-		return fmt.Errorf("required field domain.Id empty for chain %v", c.Id)
+		return fmt.Errorf("required field domain.sygmaId empty for chain %v", c.Id)
 	}
 	if c.Endpoint == "" {
 		return fmt.Errorf("required field chain.Endpoint empty for chain %v", *c.Id)
