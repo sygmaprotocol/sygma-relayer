0272fedcfd8b87fd9b2102ef7020c80130cf1d6c
tcar <tcar121293@gmail.com>
2024-07-31 14:49:45 +0200

    use backoff package;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- chains/btc/uploader/ipfs.go
+++ chains/btc/uploader/ipfs.go
@@ -3,12 +3,16 @@ package uploader
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"time"
 
+	"github.com/rs/zerolog/log"
+
 	"github.com/ChainSafe/sygma-relayer/config/relayer"
+	"github.com/cenkalti/backoff/v4"
 )
 
 const MAX_RETRIES = 3
@@ -55,31 +59,47 @@ func (s *IPFSUploader) Upload(dataToUpload []map[string]interface{}) (string, er
 	req.Header.Add("Authorization", "Bearer "+s.config.AuthToken)
 	req.Header.Add("Content-Type", writer.FormDataContentType())
 
-	var resp *http.Response
-	for attempt := 1; attempt <= MAX_RETRIES; attempt++ {
-		client := &http.Client{}
-		resp, err = client.Do(req)
-		if err == nil && resp.StatusCode == 200 {
-			break
-		}
-
-		time.Sleep(time.Duration(attempt) * time.Second)
-	}
-
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
 	var ipfsResponse IPFSResponse
-	if err := json.Unmarshal(respBody, &ipfsResponse); err != nil {
+
+	// Define the operation to be retried
+	operation := func() error {
+		return s.performRequest(req, &ipfsResponse)
+	}
+	expBackoff := backoff.NewExponentialBackOff()
+	expBackoff.MaxElapsedTime = time.Minute
+
+	notify := func(err error, duration time.Duration) {
+		log.Warn().Err(err).Msg("Unable to upload metadata to ipfs")
+	}
+
+	err = backoff.RetryNotify(operation, backoff.WithMaxRetries(expBackoff, 5), notify)
+	if err != nil {
 		return "", err
 	}
 
 	return ipfsResponse.IpfsHash, nil
+}
+
+func (s *IPFSUploader) performRequest(req *http.Request, ipfsResponse *IPFSResponse) error {
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return errors.New("received non-200 status code")
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(respBody, &ipfsResponse); err != nil {
+		return err
+	}
+
+	return nil
 }
